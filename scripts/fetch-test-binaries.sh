f1e4e22a9bf6b0685dc2a28ca3e29e9a40ef0db4
Andi Bräu <andibraeu@users.noreply.github.com>
2026-03-02 22:25:35 +0100

    remove unused  kubebuilder script

--- scripts/fetch-test-binaries.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-os=$(uname -s)
-curl -sSL  https://storage.googleapis.com/kubebuilder-tools/kubebuilder-tools-1.15.5-"$(echo "$os" | tr '[:upper:]' '[:lower:]')"-amd64.tar.gz | tar -zvxf -
-
